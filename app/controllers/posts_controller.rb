6b15d51fde76e3e90e561d90a71327a530e4243e
Ryosuke Kawauchi <ryosukekawauchi@kawauchiryouyuu-no-MacBook-Air.local>
2017-11-14 21:02:37 +0900

    Create create action, view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,13 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.permit(:image, :text)
+  end
 end
